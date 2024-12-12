343d4d1b008bc9e7bfe0e32a408a7fef09728bf4
Jethrolopwus <lopwusjethro92@gmail.com>
2024-12-12 07:06:04 +0100

    UPDATE: update layout and create navbar and footer

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SideMenu from "./components/SideMenu";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function Layout () {
+function Layout() {
   return (
-    <div className="flex">
-      <SideMenu/>
-      <main>
+    <div className="grid grid-rows-layout grid-cols-[250px_1fr] h-screen">
+    
+      <header className="row-span-1 col-span-full bg-green-600 text-white">
+        <Navbar />
+      </header>
+
+    
+      <aside className="row-span-2 col-span-1 bg-green-600  text-white">
+        <SideMenu />
+      </aside>
+
+     
+      <main className="row-span-2 col-span-1 bg-green-600 p-4 overflow-y-auto">
         <Outlet />
       </main>
+      <footer className="row-span-1 col-span-full bg-green-600 border ">
+        <Footer/>
+      </footer>
     </div>
   );
-};
+}
 
 export default Layout;
